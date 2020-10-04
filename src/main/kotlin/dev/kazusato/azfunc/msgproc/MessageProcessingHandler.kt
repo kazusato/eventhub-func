228db20d2a94ff06754164eb145633f6934ce96e
Kazuhiko Sato <super80@gmail.com>
2020-10-05 05:22:19 +0900

    enabled Event Hub trigger

--- src/main/kotlin/dev/kazusato/azfunc/msgproc/MessageProcessingHandler.kt
+++ src/main/kotlin/dev/kazusato/azfunc/msgproc/MessageProcessingHandler.kt
@@ -1,26 +1,36 @@
 package dev.kazusato.azfunc.msgproc
 
 import com.microsoft.azure.functions.*
-import com.microsoft.azure.functions.annotation.AuthorizationLevel
-import com.microsoft.azure.functions.annotation.FunctionName
-import com.microsoft.azure.functions.annotation.HttpTrigger
+import com.microsoft.azure.functions.annotation.*
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler
 import java.util.*
 
 class MsgProcHandler : AzureSpringBootRequestHandler<MsgReq, MsgResp>() {
 
-	@FunctionName("MsgProc")
-	fun exec(
-			@HttpTrigger(name = "req", methods = [HttpMethod.POST], authLevel = AuthorizationLevel.ANONYMOUS)
-			req: HttpRequestMessage<Optional<MsgReq>>,
-			ctx: ExecutionContext
-	): HttpResponseMessage {
-		ctx.logger.info("FUNCTION INPUT: ${req.body.get()}")
+//	@FunctionName("MsgProc")
+//	fun exec(
+//			@HttpTrigger(name = "req", methods = [HttpMethod.POST], authLevel = AuthorizationLevel.ANONYMOUS)
+//			req: HttpRequestMessage<Optional<MsgReq>>,
+//			ctx: ExecutionContext
+//	): HttpResponseMessage {
+//		ctx.logger.info("FUNCTION INPUT: ${req.body.get()}")
+//
+//		return req.createResponseBuilder(HttpStatus.OK)
+//				.body(handleRequest(req.body.get(), ctx))
+//				.header("Content-Type", "application/json")
+//				.build()
+//	}
 
-		return req.createResponseBuilder(HttpStatus.OK)
-				.body(handleRequest(req.body.get(), ctx))
-				.header("Content-Type", "application/json")
-				.build()
+	@FunctionName("MsgProc")
+	fun exec2(
+			@EventHubTrigger(name = "req", eventHubName = "testhub", connection = "myconn",
+					consumerGroup = "\$Default", cardinality = Cardinality.ONE)
+			req: MsgReq,
+			ctx: ExecutionContext
+	) {
+		ctx.logger.info("FUNCTION INPUT: ${req}")
+		val result = handleRequest(req, ctx)
+		ctx.logger.info("RESULT: ${result}")
 	}
 
 }
