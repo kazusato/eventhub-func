f41bc895b239e27da9624fdb910ac2ba6857a049
Kazuhiko Sato <super80@gmail.com>
2020-10-04 19:46:11 +0900

    Changed service name.

--- src/main/kotlin/dev/kazusato/azfunc/msgproc/MyMsgProc.kt
+++ src/main/kotlin/dev/kazusato/azfunc/msgproc/MyMsgProc.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service
 import java.util.function.Function
 
 @Service
-class MsgProcService : Function<MsgReq, MsgResp> {
+class MyMsgProc : Function<MsgReq, MsgResp> {
 
 	override fun apply(req: MsgReq): MsgResp {
 		return MsgResp("Welcome, ${req.text}")
