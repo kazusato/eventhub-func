fb4ab46f428822fc226c93e871c0598c150765e1
Kazuhiko Sato <super80@gmail.com>
2020-10-04 20:08:35 +0900

    Multiple beans with the same req/resp types result in "No function defined."

---
+++ src/main/kotlin/dev/kazusato/azfunc/msgproc/MyMsgProc2.kt
@@ -0,0 +1,13 @@
+package dev.kazusato.azfunc.msgproc
+
+import org.springframework.stereotype.Service
+import java.util.function.Function
+
+@Service
+class MyMsgProc2 : Function<MsgReq, MsgResp> {
+
+	override fun apply(req: MsgReq): MsgResp {
+		return MsgResp("Hello, ${req.text}")
+	}
+
+}
